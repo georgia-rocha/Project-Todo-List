1fa370e65e29a527bd3914dcf5c25f67f544b316
Georgia <georgiab543@gmail.com>
2023-01-04 00:30:49 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- todo-list/src/Componentes/Form.js
+++ todo-list/src/Componentes/Form.js
@@ -46,19 +46,28 @@ class Form extends React.Component {
             if (itemSelecionado) {
                 itemSelecionado.className = "item"
             }
-            target.className = "item-selecionado";
+            target.className = "item item-selecionado";
         } else if (target.className === "item-selecionado") {
             target.className = "item"
-            console.log("aa");
         };
     };
 
     dubleClick = ({ target }) => {
-        if (target.className === "item" || target.className === "item-selecionado") {
+        if (target.className === "item" || target.className === "item item-selecionado") {
             target.className = "item completed"
         } else {
             target.className = "item"
         }
+    };
+
+    removeSelect = () => {
+        const itemSelect = document.querySelectorAll(".item");
+        const verifySelect = [...itemSelect].filter((item) => item.className !== "item item-selecionado");
+        const filterSelect = verifySelect.map((item) => item.innerText);
+        console.log(filterSelect);
+        this.setState({
+            savedList: filterSelect,
+        }, this.saveLocalStorage);
     };
 
     removeList = () => {
@@ -68,7 +77,7 @@ class Form extends React.Component {
         }, this.saveLocalStorage);
     };
 
-    removeItensFinist = () => {
+    removeFinalized = () => {
         const { savedList } = this.state;
         const itens = document.querySelectorAll(".item");
         console.log(itens);
@@ -80,23 +89,54 @@ class Form extends React.Component {
         }, this.saveLocalStorage);
     };
 
+    moverParaBaixo = () => {
+        const itens = document.querySelectorAll(".item");
+        const itemSelect = document.querySelector(".item-selecionado");
+        if (!itemSelect) {
+            alert("Selecione uma tarefa")
+        } else {
+            const proximoItem = itemSelect.nextElementSibling;
+            if (itemSelect !== itens.lastChild) {
+                proximoItem.insertAdjacentElement('afterend', itemSelect);
+            };
+        };
+    };
+
+    moveParaCima = () => {
+        const itens = document.querySelectorAll(".item");
+        const itemSelect = document.querySelector(".item-selecionado");
+        if (!itemSelect) {
+            alert("Selecione uma tarefa")
+        } else {
+            const itemAnterior = itemSelect.previousElementSibling;
+            if (itemSelect !== itens.firstChild) {
+                itemAnterior.insertAdjacentElement('beforebegin', itemSelect);
+            };
+        };
+    };
+
     render() {
         const { inputTarefa, savedList } = this.state;
         return (
             <>
-                <header>Minha Lista de Tarefas</header>
-                <p id="funcionamento">Clique duas vezes em um item para marcá-lo como completo</p>
-                <input id="texto-tarefa" value={inputTarefa} type="text" name="inputTarefa" onChange={this.handleChange} />
-                <button type="button" id="criar-tarefa" onClick={this.handleClick}>Adicionar Tarefa</button>
-                <ol id="lista-tarefas">
-                    {savedList.length > 0 && savedList.map((list) => (
-                        <li key={list} className="item" onClick={this.onClickLi} onDoubleClick={this.dubleClick}>
-                            {list}
-                        </li>
-                    ))}
-                </ol>
-                <button type="button" id="apaga-tudo" onClick={this.removeList}>Apagar Lista</button>
-                <button type="button" id="remover-finalizados" onClick={this.removeItensFinist}> Apagar Finalizados</button>
+                    <header>Minha Lista de Tarefas</header>
+                    <p id="funcionamento">Clique duas vezes em um item para marcá-lo como completo</p>
+                    <input id="texto-tarefa" value={inputTarefa} type="text" name="inputTarefa" onChange={this.handleChange} />
+                    <button type="button" id="criar-tarefa" onClick={this.handleClick}>Adicionar Tarefa</button>
+                    <ol id="lista-tarefas">
+                        {savedList.length > 0 && savedList.map((list) => (
+                            <li key={list} className="item" onClick={this.onClickLi} onDoubleClick={this.dubleClick}>
+                                {list}
+                            </li>
+                        ))}
+                    </ol>
+                    <div id="buttons">
+                        <button type="button" id="mover-cima" onClick={this.moveParaCima}>subir</button>
+                        <button type="button" id="mover-baixo" onClick={this.moverParaBaixo}>descer</button>
+                        <button type="button" id="remover-selecionado" onClick={this.removeSelect}>Remover Item</button>
+                        <button type="button" id="apaga-tudo" onClick={this.removeList}>Remover Lista</button>
+                        <button type="button" id="remover-finalizados" onClick={this.removeFinalized}> Remover Finalizados</button>
+                    </div>
             </>
         );
     }
