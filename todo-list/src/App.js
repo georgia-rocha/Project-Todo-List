266c77422a355498b227d7e963e4e02711099c5d
Georgia <georgiab543@gmail.com>
2023-01-10 13:09:06 -0300

    ajustando

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Form from './Componentes/Form';
+import Footer from './Componentes/Footer';
 
 class App extends React.Component {
   render() {
     return(
       <main>
         <Form />
+        <Footer />
       </main>
     );
   }
